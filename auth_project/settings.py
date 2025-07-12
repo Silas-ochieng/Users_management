9d193c16fd58718b569ba1e7398691da147fce3d
Silas Ochieng <silaoketch2021@gmail.com>
2025-07-12 14:20:20 +0300

    modified

--- auth_project/settings.py
+++ auth_project/settings.py
@@ -63,12 +63,14 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')  # Correct method call
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')  # Also corrected
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 
 # Email Display Settings
-DEFAULT_FROM_EMAIL = f"User Portal Support <{EMAIL_HOST_USER}>"
+DEFAULT_FROM_EMAIL = f"User  Portal Support <{EMAIL_HOST_USER}>"
 SERVER_EMAIL = EMAIL_HOST_USER
+
 
 
 # Redirect URLs
